c0eb2407f8b8a7318db31f9a8c9ed111d1f8d926
Alexis LEON <alexis@MacBook-Pro-de-Alexis.local>
2023-06-26 10:26:38 +0200

    Correction EF

--- academie_francaise/src/components/EfQuestions.js
+++ academie_francaise/src/components/EfQuestions.js
@@ -1,34 +1,33 @@
 function EfQuestion({ ennonce, reponse, motErreur }) {
-    const mots = ennonce.split(' ');
-    const msgPasErreur = "Pas de faute"
+  const mots = ennonce.split(' ');
+  const msgPasErreur = "Pas de faute"
 
-    return (
-      <div>
-        {mots.map((mot, index) => (
-          <span key={index} onClick={() => verifMot(mot, motErreur) ? console.log("Bonne réponse") : console.log("Mauvaise réponse")}>
-            {mot}{' '}
-          </span>
-        ))}
-        <button onClick={() => verifMot(msgPasErreur, motErreur) ? console.log("Bonne réponse") : console.log("Mauvaise réponse")}>{msgPasErreur}</button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      {mots.map((mot, index) => (
+        <span key={index} onClick={() => verifMot(mot, motErreur) ? console.log("Bonne réponse") : console.log("Mauvaise réponse")}>
+          {mot}{' '}
+        </span>
+      ))}
+      <button onClick={() => verifMot(msgPasErreur, motErreur) ? console.log("Bonne réponse") : console.log("Mauvaise réponse")}>{msgPasErreur}</button>
+    </div>
+  );
+}
 
-  function verifMot(mot, motErreur) {
-    console.log(`Mot cliqué: ${mot}`);
-    if (motErreur != "Pas de faute") {
-        if (mot.includes(motErreur)) {
-            return true;
-        }
-    } else {
-        if (mot == motErreur) {
-            return true;
-        }
+function verifMot(mot, motErreur) {
+  console.log(`Mot cliqué: ${mot}`);
+  if (motErreur !== "Pas de faute") {
+    if (mot.includes(motErreur)) {
+      return true;
     }
-    return false;
+  } else {
+    if (mot === motErreur) {
+      return true;
+    }
   }
+  return false;
+}
 
 
-  
-  export default EF_question;
-  
+
+export default EfQuestion;
