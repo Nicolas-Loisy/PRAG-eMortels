c88d9a404973213a0c0e5d2d0496ec0956706b87
Alexis LEON <alexis@MacBook-Pro-de-Alexis.local>
2023-06-29 09:51:01 +0200

    Debut accueil

--- academie_francaise/src/components/BlocChoix.js
+++ academie_francaise/src/components/BlocChoix.js
@@ -1,13 +1,15 @@
 import "../css/BlocChoix.css";
 
 function BlocChoix({ titre, url, description }) {
+  const handleClick = () => {
+    window.location.href = url;
+  };
+
   return (
-    <a href={url}>
-      <div className="BlocChoix">
-        <h3>{titre}</h3>
-        <p>{description}</p>
-      </div>
-    </a>
+    <div className="BlocChoix" onClick={handleClick}>
+      <h3>{titre}</h3>
+      <p>{description}</p>
+    </div>
   );
 }
 
--- academie_francaise/src/pages/Accueil.js
+++ academie_francaise/src/pages/Accueil.js
@@ -1,18 +1,19 @@
 import BlocChoix from "../components/BlocChoix";
 import Content from "../components/Content";
+import "../css/Accueil.css";
 
 function Accueil({ }) {
   return (
     <Content>
 
       <h1>L'ACADEMIE DE RENART</h1>
-      <div>
+      <div className="optionContainer">
         <BlocChoix
           titre="Rechercher un thème"
           url="/categorie"
           description="Je souhaite m'entrainer sur une thèmatique précise."
         />
-        
+
         <BlocChoix
           titre="Questions aléatoires"
           url="/choix-niveau"
