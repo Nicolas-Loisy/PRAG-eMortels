a3aa1ae5430ec857ad172138dc4615ca90a5bb93
Alexis LEON <alexis@MacBook-Pro-de-Alexis.local>
2023-06-30 21:34:21 +0200

    finition trou

--- academie_francaise/src/components/TrouQuestion.js
+++ academie_francaise/src/components/TrouQuestion.js
@@ -1,48 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/TrouQuestion.css';
 
-function TrouQuestion({ ennonce, reponse, repondu, onUserResponse }) {
+function TrouQuestion({ ennonce, reponse, repondu, onUserResponse, reponseUtilisateur }) {
   const regex = /\.{2,}/g;
   const [inputValue, setInputValue] = useState('');
   const ennonceUpdated = remplacePointsParInput(ennonce);
   const [corrige, setCorrige] = useState('');
 
+  useEffect(() => {
+    if (repondu === true) {
+      setCorrige(
+        <p dangerouslySetInnerHTML={{ __html: ennonce.replace(regex, `<span class="reponseUtilisateur correct">${reponseUtilisateur}</span>`) }} />
+      );
+    } else {
+      setCorrige(
+        <>
+          <p dangerouslySetInnerHTML={{ __html: ennonce.replace(regex, `<span class="reponseUtilisateur incorrect">${reponseUtilisateur}</span>`) }} />
+          <p className='correction' dangerouslySetInnerHTML={{ __html: ennonce.replace(regex, `<span class="correct">${reponse}</span>`) }} />
+        </>
+      );
+    }
+  }, [ennonce, repondu]);
+
   // Actions effectuées en cliquant sur "Valider"
   function handleClick() {
     if (repondu === null) {
       if (inputValue === reponse) {
+        onUserResponse(true, inputValue);
         setCorrige(
           <p dangerouslySetInnerHTML={{ __html: ennonce.replace(regex, `<span class="reponseUtilisateur correct">${inputValue}</span>`) }} />
         );
-        onUserResponse(true, inputValue);
       } else {
+        onUserResponse(false, inputValue);
         setCorrige(
           <>
             <p dangerouslySetInnerHTML={{ __html: ennonce.replace(regex, `<span class="reponseUtilisateur incorrect">${inputValue}</span>`) }} />
             <p className='correction' dangerouslySetInnerHTML={{ __html: ennonce.replace(regex, `<span class="correct">${reponse}</span>`) }} />
           </>
         );
-        onUserResponse(false, inputValue);
       }
       setInputValue('');
     }
   }
 
-  // Remplace les points en input
   function remplacePointsParInput(str) {
     const parts = str.split(regex);
     const elements = parts.map((part, index) => {
       return (
         <React.Fragment key={`element-${index}`}>
-          {part}
-          {index !== parts.length - 1 && <input type="text" className="user-input" value={inputValue} onChange={(event) => setInputValue(event.target.value)} />}
+          <span dangerouslySetInnerHTML={{ __html: part }} />
+          {index !== parts.length - 1 && (
+            <input type="text" className="user-input" value={inputValue} onChange={(event) => setInputValue(event.target.value)} />
+          )}
         </React.Fragment>
       );
     });
-
+  
     return elements;
   }
-  
 
   return (
     <div className='TrouQuestion'>
@@ -52,7 +67,7 @@ function TrouQuestion({ ennonce, reponse, repondu, onUserResponse }) {
             {ennonceUpdated}
           </p>
           <div className='validation'>
-            <button onClick={handleClick}>Valider</button>
+            <div className="button" onClick={handleClick}>Valider</div>
           </div>
         </div>
       ) : (
