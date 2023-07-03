a8c6aa7e9ed4a5185a04aa5b8c5440a353993e3c
Alexis LEON <alexis@MacBook-Pro-de-Alexis.local>
2023-07-03 11:56:45 +0200

    Correction bug de nicolas

--- academie_francaise/src/pages/ChoixExercice.js
+++ academie_francaise/src/pages/ChoixExercice.js
@@ -18,7 +18,7 @@ function ChoixExercice() {
 
   const fetchData = async () => {
     try {
-      const niveauxData = await api.getNiveaux(params.categorie, params.sousCategorie);
+      const niveauxData = await api.getNiveauxTypesExo(params.categorie, params.sousCategorie);
       setNiveaux(niveauxData);
     } catch (error) {
       console.error(error);
