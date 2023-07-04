f3c13ec091a5daedd21cea7e4169fb07ca0d451d
Alexis LEON <alexis@MacBook-Pro-de-Alexis.local>
2023-07-04 15:21:17 +0200

    nettoyage Exercice
    On pleure du sang mais on est là

---
+++ academie_francaise/src/components/Question.js
@@ -0,0 +1,61 @@
+import React, { useCallback } from 'react';
+
+import QcmQuestion from './QcmQuestion';
+import SubstitutionQuestion from './SubstitutionQuestion';
+import TrouQuestion from './TrouQuestion';
+
+import '../css/Question.css';
+
+function Question({ question, index, handleUserResponse }) {
+  // Création des composants HTML contenant les questions
+  const renderQuestionComponent = useCallback(() => {
+    switch (question.type) {
+      case 'QCM':
+        return (
+          <QcmQuestion
+            key={index}
+            ennonce={question.question}
+            reponses={question.reponses}
+            repondu={question.repondu}
+            onUserResponse={(isCorrect, reponseUtilisateur) =>
+              handleUserResponse(index, isCorrect, reponseUtilisateur)
+            }
+            reponseUtilisateur={question.reponseUtilisateur}
+          />
+        );
+      case 'phraseTrous':
+        return (
+          <TrouQuestion
+            key={index}
+            ennonce={question.question}
+            reponse={question.reponse}
+            repondu={question.repondu}
+            onUserResponse={(isCorrect, reponseUtilisateur) =>
+              handleUserResponse(index, isCorrect, reponseUtilisateur)
+            }
+            reponseUtilisateur={question.reponseUtilisateur}
+          />
+        );
+      case 'substitution':
+        return (
+          <SubstitutionQuestion
+            key={index}
+            ennonce={question.question}
+            reponse={question.reponse}
+            repondu={question.repondu}
+            onUserResponse={(isCorrect, reponseUtilisateur) =>
+              handleUserResponse(index, isCorrect, reponseUtilisateur)
+            }
+            reponseUtilisateur={question.reponseUtilisateur}
+          />
+        );
+      default:
+        console.log('Type de question non pris en charge: ' + question.type);
+        return null;
+    }
+  }, [question, index, handleUserResponse]);
+
+  return <>{renderQuestionComponent()}</>;
+}
+
+export default Question;
--- academie_francaise/src/pages/Exercice.js
+++ academie_francaise/src/pages/Exercice.js
@@ -1,8 +1,7 @@
 import { useState, useEffect, useCallback } from 'react';
 import { useParams } from "react-router-dom";
 
-import QcmQuestion from "../components/QcmQuestion";
-import TrouQuestion from "../components/TrouQuestion";
+import Question from '../components/Question';
 import Content from "../components/Content"
 import Tag from "../components/Tag";
 import Numerotation from "../components/Numerotation";
@@ -11,17 +10,17 @@ import Recap from '../components/Recap';
 import { api } from "../api/Api";
 
 import '../css/Exercice.css';
-import SubstitutionQuestion from '../components/SubstitutionQuestion';
 
 function Exercice() {
   const params = useParams();
   const [exercice, setExercice] = useState(null);
-  const [renderedQuestions, setRenderedQuestions] = useState([]);
   const [questionCourante, setQuestionCourante] = useState(0);
   const [voirRecap, setVoirRecap] = useState(false);
 
+  // Extraction des données de la BDD
   const fetchData = useCallback(async () => {
     try {
+      // Etape 1 : Requête API
       const exerciceData = await api.getExercice(
         params.categorie,
         params.sousCategorie,
@@ -29,116 +28,79 @@ function Exercice() {
         params.exercice
       );
 
-      const updatedQuestions = exerciceData.exercice[0].questions.map((question) => ({
+      // Etape 2 : Ajout du champ repondu = null
+      const updatedQuestions = exerciceData.exercice.questions.map((question) => ({
         ...question,
         repondu: null,
       }));
 
+      // Etape 3 : Mise à jour de la liste des questions de l'exercice
       const updatedExercice = {
-        ...exerciceData.exercice[0],
+        ...exerciceData.exercice,
         questions: updatedQuestions,
       };
 
+      // Etape 4 : Mise à jour de la valeur de la variable exercice
       setExercice({
         ...exerciceData,
-        exercice: [updatedExercice],
+        exercice: updatedExercice,
       });
 
     } catch (error) {
       console.error(error);
     }
-  }, [params]);
+  }, [params]); // Si params change alors fetchData est actualisé
 
+
+  // Enregistrer la réponse d'un utilisateur
+  const handleUserResponse = (index, isCorrect, reponseUtilisateur) => {
+    setExercice((prevExercice) => {
+      // récupération de la variable exercice
+      const { exercice } = prevExercice;
+
+      // Copie des questions existantes avec le spread operator
+      const updatedQuestions = [...exercice.questions];
+
+      // Enregistrement de la réponse de l'utilisateur
+      const updatedQuestion = {
+        ...updatedQuestions[index],
+        repondu: isCorrect,
+        reponseUtilisateur: reponseUtilisateur,
+      };
+
+      // Remplace la question existante par la nouvelle question mise à jour
+      updatedQuestions[index] = updatedQuestion;
+
+      // Création de l'objet updatedExercice avec les questions mises à jour
+      const updatedExercice = {
+        ...exercice,
+        questions: updatedQuestions,
+      };
+
+      // Retour de l'exercice mis à jour dans l'objet retourné par setExercice
+      return {
+        ...prevExercice,
+        exercice: updatedExercice,
+      };
+    });
+  };
+
+  // Afficher la question sélectionnée
+  const handleClickQuestion = (numQuestion) => {
+    setVoirRecap(false); // Masquer le résumé de fin d'exercice
+    setQuestionCourante(numQuestion); // Modifier la question à afficher
+  };
+
+  // Effectue une requète API lorsque params change
   useEffect(() => {
     fetchData();
   }, [fetchData, params]);
 
-  useEffect(() => {
-    if (exercice !== null && exercice.exercice[0].questions) {
-      const updatedRenderedQuestions = exercice.exercice[0].questions.map((question, index) => {
-        switch (question.type) {
-          case "QCM":
-            return (
-              <QcmQuestion
-                key={index}
-                ennonce={question.question}
-                reponses={question.reponses}
-                repondu={question.repondu}
-                onUserResponse={(isCorrect, reponseUtilisateur) =>
-                  handleUserResponse(index, isCorrect, reponseUtilisateur)
-                }
-                reponseUtilisateur={question.reponseUtilisateur}
-              />
-            );
-          case "phraseTrous":
-            return (
-              <TrouQuestion
-                key={index}
-                ennonce={question.question}
-                reponse={question.reponse}
-                repondu={question.repondu}
-                onUserResponse={(isCorrect, reponseUtilisateur) =>
-                  handleUserResponse(index, isCorrect, reponseUtilisateur)
-                }
-                reponseUtilisateur={question.reponseUtilisateur}
-              />
-            );
-          case "substitution":
-            return (
-              <SubstitutionQuestion
-                key={index}
-                ennonce={question.question}
-                reponse={question.reponse}
-                repondu={question.repondu}
-                onUserResponse={(isCorrect, reponseUtilisateur) =>
-                  handleUserResponse(index, isCorrect, reponseUtilisateur)
-                }
-                reponseUtilisateur={question.reponseUtilisateur}
-              />
-            );
-          default:
-            console.log("Type de question non pris en charge: " + question.type);
-            return null;
-        }
-      });
-
-      setRenderedQuestions(updatedRenderedQuestions);
-    }
-  }, [exercice]);
-
-  const handleClickQuestion = (numQuestion) => {
-    setVoirRecap(false);
-    setQuestionCourante(numQuestion);
-  };
-
-  const handleUserResponse = (index, isCorrect, reponseUtilisateur) => {
-    setExercice((prevExercice) => {
-      const updatedExercice = {
-        ...prevExercice,
-        exercice: [
-          {
-            ...prevExercice.exercice[0],
-            questions: prevExercice.exercice[0].questions.map((question, i) => {
-              if (i === index) {
-                return {
-                  ...question,
-                  repondu: isCorrect,
-                  reponseUtilisateur: reponseUtilisateur,
-                };
-              }
-              return question;
-            }),
-          },
-        ],
-      };
-      return updatedExercice;
-    });
-  };
-
   return (
     <Content>
       <div className="Exercice">
-        {/* Affichage des tags identifiants l'exercice */}
+
+        {/* Tags d'identifications de l'exercice */}
         {exercice &&
           <div className="TagList">
             <div className="col_1">
@@ -161,85 +123,87 @@ function Exercice() {
           </div>
         }
 
-
         <div className="col_container">
           {/* Affichage de la mascotte et des règles de français */}
           <div className="col_1">
             <div className="Mascotte"></div>
           </div>
 
-          {/* Contenu principal de l'exercice */}
           <div className="col_2">
             {exercice && (
               <>
-                {voirRecap ? (
+                {/* Affichage classique (hors récap) */}
+                {!voirRecap ? (
+                  <>
+                    {/* Contenu de l'exercice */}
+                    <div>
+                      <p className="intitule">{exercice.exercice.intitule}</p>
+                      <Question
+                        question={exercice.exercice.questions[questionCourante]}
+                        index={questionCourante}
+                        handleUserResponse={handleUserResponse}
+                      />
+                    </div>
+          
+                    {/* Navigation (précédent / suivant) */}
+                    <div className="Navigation tagCliquable">
+                      {questionCourante > 0 && (
+                        <div>
+                          <Tag className="Cliquable">
+                            <p onClick={() => setQuestionCourante((prevQuestionCourante) => prevQuestionCourante - 1)}>
+                              Précédent
+                            </p>
+                          </Tag>
+                        </div>
+                      )}
+
+                      {exercice.exercice.questions[questionCourante].repondu !== null && questionCourante < exercice.exercice.questions.length - 1 && (
+                        <div>
+                          <Tag className="Cliquable">
+                            <p onClick={() => setQuestionCourante((prevQuestionCourante) => prevQuestionCourante + 1)}>
+                              Suivant
+                            </p>
+                          </Tag>
+                        </div>
+                      )}
+                    </div>
+
+                    {/* Affichage du bouton résumé */}
+                    {exercice.exercice.questions.every((question) => question.repondu !== null) && (
+                      <div className="tagCliquable">
+                        <Tag className="Cliquable">
+                          <p
+                            onClick={() => {
+                              setVoirRecap(true);
+                              setQuestionCourante(-1);
+                            }}
+                          >
+                            Résumé
+                          </p>
+                        </Tag>
+                      </div>
+                    )}
+                  </>
+                ) : (
+
+                  /* Affichage du score du joueur */
                   <div className="Recap">
-                    <Recap questions={exercice.exercice[0].questions} />
+                    <Recap questions={exercice.exercice.questions} />
                     <div>
                       <Tag className="Cliquable">
                         <p onClick={() => { window.location.href = "/" }}>Accueil</p>
                       </Tag>
                     </div>
                   </div>
-                ) : (
-                  <div>
-                    <p className="intitule">{exercice.exercice[0].intitule}</p>
-                    {renderedQuestions.length > 0 && renderedQuestions[questionCourante]}
-                  </div>
                 )}
 
-                {!voirRecap && (
-                  <div className="Navigation tagCliquable">
-                    {questionCourante > 0 && (
-                      <div>
-                        <Tag className="Cliquable">
-                          <p onClick={() => setQuestionCourante((prevQuestionCourante) => prevQuestionCourante - 1)}>
-                            Précédent
-                          </p>
-                        </Tag>
-                      </div>
-                    )}
+                {/* Navigation entre les questions (numéros) */}
+                <Numerotation
+                  questions={exercice.exercice.questions}
+                  questionCourante={questionCourante}
+                  onClick={handleClickQuestion}
+                />
 
-                    {exercice.exercice[0].questions[questionCourante].repondu !== null && questionCourante < exercice.exercice[0].questions.length - 1 && (
-                      <div>
-                        <Tag className="Cliquable">
-                          <p onClick={() => setQuestionCourante((prevQuestionCourante) => prevQuestionCourante + 1)}>
-                            Suivant
-                          </p>
-                        </Tag>
-                      </div>
-                    )}
-                  </div>
-                )}
-
-                {exercice.exercice[0].questions.every((question) => question.repondu !== null) && !voirRecap && (
-                  <div className="tagCliquable">
-                    <Tag className="Cliquable">
-                      <p
-                        onClick={() => {
-                          setVoirRecap(true);
-                          setQuestionCourante(-1);
-                        }}
-                      >
-                        Résumé
-                      </p>
-                    </Tag>
-                  </div>
-                )}
-
-                <div className="pagination">
-                  {exercice.exercice[0].questions.map((question, i) =>
-                  (
-                    <Numerotation
-                      key={i}
-                      Num={i}
-                      isSelected={i === questionCourante}
-                      onClick={() => handleClickQuestion(i)}
-                      repondu={question.repondu}
-                    />
-                  )
-                  )}
-                </div>
               </>
             )}
           </div>
