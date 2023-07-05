b8c540a6476ce29865c9095e6613bf043c13104b
Alexis LEON <alexis@MacBook-Pro-de-Alexis.local>
2023-07-05 10:01:33 +0200

    création composant bulle

--- academie_francaise/src/pages/Accueil.js
+++ academie_francaise/src/pages/Accueil.js
@@ -1,12 +1,10 @@
 import Content from "../components/Content";
 import logo from "../ressources/Images/Logo.png";
 import "../css/Accueil.css";
+import Bouton from "../components/Bouton";
+import Bulle from "../components/Bulle";
 
 function Accueil() {
-  const handleClick = (url) => {
-    window.location.href = url;
-  };
-
   return (
     <Content>
       <div className="Accueil">
@@ -22,20 +20,24 @@ function Accueil() {
           </div>
 
           <div className="Col2">
-            <div className="BulleBD">
-              <p>Bienvenue dans mon académie ! <br /><br />
-                Ici, vous trouverez plusieurs types d’exercices sur différents thèmes pour voir ou revoir quelques règles de la langue française.<br />
+            <Bulle>
+              <h2>Bienvenue dans mon académie !</h2>
+              <p>  Ici, vous trouverez plusieurs types d’exercices sur différents thèmes pour voir ou revoir quelques règles de la langue française.<br />
                 Je suis Maître Renart et je pourrai vous aider à chaque question pour vous rappeler la règle que vous devez appliquer, il suffit de cliquer sur moi pour me demander !<br /><br />
                 Ici, tu peux parcourir le catalogue à la recherche d'un thème en particulier ou tu peux t'entrainer sur des exercices de ma sélection.
               </p>
-            </div>
+            </Bulle>
             <div className="Navigation">
-              <div className="Button" onClick={() => handleClick("/catalogue")}>
-                <p>Mode Catalogue</p>
-              </div>
-              <div className="Button" onClick={() => handleClick("/entrainement")}>
-                <p>Mode Entrainement</p>
-              </div>
+              <Bouton
+                nom="Mode Catalogue"
+                url="/catalogue"
+                version="Primaire"
+              />
+              <Bouton
+                nom="Mode Entrainement"
+                url="/entrainement"
+                version="Primaire"
+              />
             </div>
           </div>
         </div>
