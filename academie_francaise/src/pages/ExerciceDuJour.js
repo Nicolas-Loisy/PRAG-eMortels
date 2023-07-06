41933fce7531b60f95598793d0f7a8bb3ca412be
Alexis LEON <alexis@MacBook-Pro-de-Alexis.local>
2023-07-06 15:08:57 +0200

    Ajout chronometre

--- academie_francaise/src/pages/ExerciceDuJour.js
+++ academie_francaise/src/pages/ExerciceDuJour.js
@@ -6,6 +6,7 @@ import Content from "../components/Content"
 import Tag from "../components/Tag";
 import Numerotation from "../components/Numerotation";
 import Recap from '../components/Recap';
+import Chronometre from '../components/Chronometre';
 
 import { api } from "../api/Api";
 
@@ -16,7 +17,11 @@ function ExerciceDuJour() {
   const [exercice, setExercice] = useState(null);
   const [questionCourante, setQuestionCourante] = useState(0);
   const [voirRecap, setVoirRecap] = useState(false);
-  const [voirExplication, setVoirExplication] = useState(false);
+  const [isTimerRunning, setIsTimerRunning] = useState(true);
+
+  const handleStopTimer = () => {
+    setIsTimerRunning(false);
+  };
 
   // Extraction des données de la BDD
   const fetchData = useCallback(async () => {
@@ -49,48 +54,50 @@ function ExerciceDuJour() {
 
 
   // Enregistrer la réponse d'un utilisateur
-  const handleUserResponse = (index, isCorrect, reponseUtilisateur) => {
-    setExercice((prevExercice) => {
-      // récupération de la variable exercice
-      const { exercice } = prevExercice;
+  // Enregistrer la réponse d'un utilisateur
+const handleUserResponse = (index, isCorrect, reponseUtilisateur) => {
+  setExercice((prevExercice) => {
+    // récupération de la variable exercice
+    const { exercice } = prevExercice;
 
-      // Copie des questions existantes avec le spread operator
-      const updatedQuestions = [...exercice.questions];
+    // Copie des questions existantes avec le spread operator
+    const updatedQuestions = [...exercice.questions];
 
-      // Enregistrement de la réponse de l'utilisateur
-      const updatedQuestion = {
-        ...updatedQuestions[index],
-        repondu: isCorrect,
-        reponseUtilisateur: reponseUtilisateur,
-      };
+    // Enregistrement de la réponse de l'utilisateur
+    const updatedQuestion = {
+      ...updatedQuestions[index],
+      repondu: isCorrect,
+      reponseUtilisateur: reponseUtilisateur,
+    };
 
-      // Remplace la question existante par la nouvelle question mise à jour
-      updatedQuestions[index] = updatedQuestion;
+    // Remplace la question existante par la nouvelle question mise à jour
+    updatedQuestions[index] = updatedQuestion;
 
-      // Création de l'objet updatedExercice avec les questions mises à jour
-      const updatedExercice = {
-        ...exercice,
-        questions: updatedQuestions,
-      };
+    // Création de l'objet updatedExercice avec les questions mises à jour
+    const updatedExercice = {
+      ...exercice,
+      questions: updatedQuestions,
+    };
 
-      // Retour de l'exercice mis à jour dans l'objet retourné par setExercice
-      return {
-        ...prevExercice,
-        exercice: updatedExercice,
-      };
-    });
-  };
+    // Retour de l'exercice mis à jour dans l'objet retourné par setExercice
+    const newExercice = {
+      ...prevExercice,
+      exercice: updatedExercice,
+    };
+
+    if (updatedExercice.questions.every((question) => question.repondu !== null)) {
+      handleStopTimer();
+    }
+
+    return newExercice;
+  });
+};
+
 
   // Afficher la question sélectionnée
   const handleClickQuestion = (numQuestion) => {
     setVoirRecap(false); // Masquer le résumé de fin d'exercice
     setQuestionCourante(numQuestion); // Modifier la question à afficher
-    setVoirExplication(false); // Masquer l'explication de l'exercice
-  };
-
-  // Afficher l'explication/règle, l'extra et le lien
-  const handleClickExplication = () => {
-    setVoirExplication(!voirExplication);
   };
 
   // Effectue une requète API lorsque params change
@@ -125,22 +132,12 @@ function ExerciceDuJour() {
             </div>
           </div>
         }
+        
+        <Chronometre onStopTimer={handleStopTimer} isRunning={isTimerRunning} />
 
         <div className="col_container">
           {/* Affichage de la mascotte et des règles de français */}
           <div className="col_1">
-            <div className="Mascotte" onClick={handleClickExplication}/>
-              {/* Affichage de la règle de français et du lien si ils existent */}
-              {voirExplication && exercice.exercice.explication && exercice.exercice.lien && (
-                    <div className='ExplicationEtLien'>
-                      <div className="Explication">
-                        <p dangerouslySetInnerHTML={{ __html: exercice.exercice.explication}}/>
-                        <div className="Lien">
-                          <a href={exercice.exercice.lien}  target="_blank" rel="noreferrer">Lien d'explication</a>
-                        </div>
-                      </div>
-                    </div>
-              )}
           </div>
 
           <div className="col_2">
@@ -151,14 +148,14 @@ function ExerciceDuJour() {
                   <>
                     {/* Contenu de l'exercice */}
                     <div>
-                      <p className="intitule" dangerouslySetInnerHTML={{ __html: exercice.exercice.intitule}}/>
+                      <p className="intitule" dangerouslySetInnerHTML={{ __html: exercice.exercice.intitule }} />
                       <Question
                         question={exercice.exercice.questions[questionCourante]}
                         index={questionCourante}
                         handleUserResponse={handleUserResponse}
                       />
                     </div>
-          
+
                     {/* Navigation (précédent / suivant) */}
                     <div className="Navigation tagCliquable">
                       {questionCourante > 0 && (
